b4464650d1140a9ba7fa9aa3feec570ae939239a
DATAKTEAM <datak.7012.team@gmail.com>
2016-03-14 21:56:27 +0330

    Update onservice.lua

--- plugins/onservice.lua
+++ plugins/onservice.lua
@@ -3,18 +3,18 @@ do
 local function run(msg, matches)
 local bot_id = our_id -- your bot id
    -- like local bot_id = 1234567
-    if matches[1] == 'leave' and is_admin(msg) then
-       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
+    if matches[1] == 'لفت بده' and is_admin(msg) then
+       chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, true)
     elseif msg.action.type == "chat_add_user" and msg.action.user.id == tonumber(bot_id) and not is_sudo(msg) then
-      send_large_msg("chat#id"..msg.to.id, 'this is not one of my groups.', ok_cb, false)
-      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, false)
-      block_user("user#id"..msg.from.id,ok_cb,false)
+      send_large_msg("chat#id"..msg.to.id, 'خسیس گدا گروه 2 تومنه مگه چیه خب بخر.', ok_cb, true)
+      chat_del_user("chat#id"..msg.to.id, 'user#id'..bot_id, ok_cb, true)
+      block_user("user#id"..msg.from.id,ok_cb,true)
     end
 end
  
 return {
   patterns = {
-    "^[!/](leave)$",
+    "^(لفت بده)$",
     "^!!tgservice (.+)$",
   },
   run = run
