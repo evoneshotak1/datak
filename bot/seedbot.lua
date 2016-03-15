e86de8f5c41b2762b44920bda24438aac2034a73
DATAKTEAM <datak.7012.team@gmail.com>
2016-03-15 04:49:28 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -240,7 +240,11 @@ function create_config( )
     "google",
     "Time",
     "tekrar",
-    "bego"
+    "bego",
+    "fada",
+    "dad",
+    "fosh",
+    "salam"
     },
     sudo_users = {159359772,185532812,111488993,80182995,173979569,tonumber(our_id)},--Sudo users
     disabled_channels = {},
