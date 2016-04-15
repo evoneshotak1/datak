77f377771639f22f591f33c3d6d7990289a643ee
DATAKTEAM <datak.7012.team@gmail.com>
2016-04-15 08:56:26 +0430

    Create datak-info.lua

---
+++ plugins/datak-info.lua
@@ -0,0 +1,24 @@
+do
+
+function run(msg, matches)
+  return [[*datak*
+  
+  *open sourced*
+  
+  *https://github.com/DATAKTM/datak*
+  
+  *Bot-Version : 2*
+  
+  *channel = @datak_team * ]]
+end
+
+return {
+  description = "Shows bot info", 
+  usage = "!version: Shows bot info",
+  patterns = {
+    "^داتک$"
+  }, 
+  run = run 
+}
+
+end
